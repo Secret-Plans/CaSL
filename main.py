d6c13100704999d31fabc651d8531c0965a12170
Secret-Plans <57553878+Secret-Plans@users.noreply.github.com>
2020-06-05 12:50:04 +1200

    Changed file loading

--- main.py
+++ main.py
@@ -3,23 +3,55 @@ from casl_parser import Parser
 import json
 import os
 
-keyword_defs = {}
+
+
+# Loads settings
+settings = {}
+with open("Data/Config.json") as f:
+    settings = json.load(f)
+
+if settings["File Loading"]["Print Settings"]:
+    print(f"Settings: {settings}")
+
+
+# Loads keywords
+keyword_defs = []
 _dir = "Data/KeywordDefs"
 for filename in os.listdir(_dir):
     with open(os.path.join(_dir, filename), "r") as f:
-        keyword_defs = {**keyword_defs, **json.load(f)}
+        keyword_defs += json.load(f)["root"]
 
+if settings["File Loading"]["Print Keywords"]:
+    print(f"Keyword Definitions: {keyword_defs}")
+
+
+# Loads symbols
 symbol_defs = {}
 _dir = "Data/SymbolDefs"
 for filename in os.listdir(_dir):
     with open(os.path.join(_dir, filename), "r") as f:
         symbol_defs = {**symbol_defs, **json.load(f)}
 
-_dir = "Data/Test Files/Test 1.casl"
-with open(_dir, "r") as f:
+if settings["File Loading"]["Print Symbols"]:
+    print(f"Symbol Definitions: {symbol_defs}")
+
+
+print("\n\n")
+
+
+# Loads file to be compiled
+with open("Data/Test Files/Test 1.casl", "r") as f:
     data = f.read()
 
-l = Lexer(keyword_defs, symbol_defs)
+
+# Lexical Analysis
+l = Lexer(keyword_defs, symbol_defs, settings["Lexer"])
 tokens = l.tokenize(data)
 
-print(tokens)
+
+# Parsing
+p = Parser(["num", "string", "bool", "list", "matrix"], settings["Parser"])
+# ast = p.parse(tokens)
+
+
+# Compilation
